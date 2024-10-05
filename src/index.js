41d9bc2a982535ae991659a257a3cb9491c96ed2
heynileshyadav <heynileshyadav@gmail.com>
2024-10-05 18:18:46 +0530

    hi

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Form, Navbar } from "react-bootstrap";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
